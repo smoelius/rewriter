2d263ac08608a8469908d68f7171a19ea70547c4
Samuel Moelius <sam@moeli.us>
2024-07-13 10:06:40 -0400

    Move `__check-proc-macro2-spans` and `offsets_from_span` into `OffsetCalculator`

--- src/offset_calculator/mod.rs
+++ src/offset_calculator/mod.rs
@@ -1,4 +1,8 @@
-use crate::span::LineColumn;
+use crate::interface::Span as _;
+use crate::span::{LineColumn, Span};
+
+#[cfg(feature = "__check-proc-macro2-spans")]
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 mod impls;
 
@@ -6,6 +10,9 @@ use impls::CachingOffsetCalculator;
 
 #[cfg(feature = "check-offsets")]
 use impls::StatelessOffsetCalculator;
+
+#[cfg(feature = "__check-proc-macro2-spans")]
+static BASE_NEXT: AtomicUsize = AtomicUsize::new(0);
 
 pub trait Interface {
     fn offset_from_line_column(&mut self, line_column: LineColumn) -> (usize, bool);
@@ -17,16 +24,49 @@ pub struct OffsetCalculator<'original> {
 
     #[cfg(feature = "check-offsets")]
     stateless: StatelessOffsetCalculator<'original>,
+
+    #[cfg(feature = "__check-proc-macro2-spans")]
+    base: usize,
 }
 
 impl<'original> OffsetCalculator<'original> {
     pub fn new(original: &'original str) -> Self {
+        #[cfg(feature = "__check-proc-macro2-spans")]
+        let base = BASE_NEXT.fetch_add(1 + original.as_bytes().len(), Ordering::SeqCst);
+
         Self {
             caching: CachingOffsetCalculator::new(original),
 
             #[cfg(feature = "check-offsets")]
             stateless: StatelessOffsetCalculator::new(original),
+
+            #[cfg(feature = "__check-proc-macro2-spans")]
+            base,
         }
+    }
+
+    pub fn offsets_from_span(&mut self, span: Span) -> (usize, usize) {
+        let (start, start_ascii) = self.offset_from_line_column(span.start());
+        let (end, end_ascii) = self.offset_from_line_column(span.end());
+
+        assert!(!end_ascii || start_ascii);
+
+        // smoelius: `proc_macro2::Span`'s debug output doesn't seem to account for UTF-8.
+        #[cfg(feature = "__check-proc-macro2-spans")]
+        if end_ascii {
+            let start = self.base + start;
+            let end = self.base + end;
+            assert_eq!(
+                format!("{span:?}"),
+                format!("bytes({}..{})", 1 + start, 1 + end),
+                "self = {:#?}, span.start() = {:?}, span.end() = {:?}",
+                self,
+                span.start(),
+                span.end(),
+            );
+        }
+
+        (start, end)
     }
 }
 
