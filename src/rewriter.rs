03a31381d277db6d04c9be9f4cc7fcdd2fb931c0
Samuel Moelius <sam@moeli.us>
2024-07-13 09:43:54 -0400

    `check-spans` -> `__check-proc-macro2-spans`

--- src/rewriter.rs
+++ src/rewriter.rs
@@ -2,10 +2,10 @@ use crate::interface::Span as _;
 use crate::offset_based_rewriter::{self, OffsetBasedRewriter};
 use crate::offset_calculator::{self, OffsetCalculator};
 use crate::span::{LineColumn, Span};
-#[cfg(feature = "check-spans")]
+#[cfg(feature = "__check-proc-macro2-spans")]
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-#[cfg(feature = "check-spans")]
+#[cfg(feature = "__check-proc-macro2-spans")]
 static BASE_NEXT: AtomicUsize = AtomicUsize::new(0);
 
 #[derive(Debug)]
@@ -13,19 +13,19 @@ pub struct Rewriter<'original> {
     line_column: LineColumn,
     offset_calculator: OffsetCalculator<'original>,
     offset_based_rewriter: OffsetBasedRewriter<'original>,
-    #[cfg(feature = "check-spans")]
+    #[cfg(feature = "__check-proc-macro2-spans")]
     base: usize,
 }
 
 impl<'original> Rewriter<'original> {
     pub fn new(original: &'original str) -> Self {
-        #[cfg(feature = "check-spans")]
+        #[cfg(feature = "__check-proc-macro2-spans")]
         let base = BASE_NEXT.fetch_add(1 + original.as_bytes().len(), Ordering::SeqCst);
         Self {
             line_column: LineColumn { line: 1, column: 0 },
             offset_calculator: OffsetCalculator::new(original),
             offset_based_rewriter: OffsetBasedRewriter::new(original),
-            #[cfg(feature = "check-spans")]
+            #[cfg(feature = "__check-proc-macro2-spans")]
             base,
         }
     }
@@ -63,7 +63,7 @@ impl<'original> Rewriter<'original> {
         let (end, end_ascii) = self.offset_calculator.offset_from_line_column(span.end());
         assert!(!end_ascii || start_ascii);
         // smoelius: `Span`'s debug output doesn't seem to account for UTF-8.
-        #[cfg(feature = "check-spans")]
+        #[cfg(feature = "__check-proc-macro2-spans")]
         if end_ascii {
             let start = self.base + start;
             let end = self.base + end;
