44831a5c6de679ddcf2fdcd1282ef4f920cb02a0
Samuel Moelius <sam@moeli.us>
2024-07-15 06:13:08 -0400

    Add `hack_feature_powerset_clippy` test

--- tests/ci.rs
+++ tests/ci.rs
@@ -2,6 +2,22 @@ use assert_cmd::assert::OutputAssertExt;
 use std::process::Command;
 
 #[test]
+fn hack_feature_powerset_clippy() {
+    Command::new("rustup")
+        .env("RUSTFLAGS", "-D warnings")
+        .args([
+            "run",
+            "nightly",
+            "cargo",
+            "hack",
+            "--feature-powerset",
+            "clippy",
+        ])
+        .assert()
+        .success();
+}
+
+#[test]
 fn hack_feature_powerset_udeps() {
     Command::new("rustup")
         .env("RUSTFLAGS", "-D warnings")
